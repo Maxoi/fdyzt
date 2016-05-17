cba6ca96dadbf03efff70298d0950cd68a07f2a8
fishermanmax <xuiihong@qq.com>
2016-05-17 14:26:32 +0800

    cms搭建

---
+++ phpsso_server/caches/caches_admin/caches_data/applist.cache.php
@@ -0,0 +1,16 @@
+<?php
+return array (
+  1 => 
+  array (
+    'appid' => '1',
+    'type' => 'phpcms_v9',
+    'name' => 'phpcms v9',
+    'url' => 'http://127.0.0.1/xsc_fdyzt/fdyzt/',
+    'authkey' => 'Ig9wZMSQ5cFqQq8TEptk5aycNIu2SRSk',
+    'ip' => '',
+    'apifilename' => 'api.php?op=phpsso',
+    'charset' => 'utf-8',
+    'synlogin' => '1',
+  ),
+);
+?>
